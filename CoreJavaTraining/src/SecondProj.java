0c87b5fc7b27de0dfa09d54ca5ea4d6dc6a10e06
Toseef Akram <toseefinterior@gmail.com>
2015-09-05 18:18:00 -0400

    Update SecondProj.java

--- CoreJavaTraining/src/SecondProj.java
+++ CoreJavaTraining/src/SecondProj.java
@@ -3,6 +3,7 @@ public class SecondProj {
 
 	public static void main(String[] args) {
 		System.out.println("This is my 1st new file");
+		System.out.println("This is my 2222st new file");
 		
 	}
 
