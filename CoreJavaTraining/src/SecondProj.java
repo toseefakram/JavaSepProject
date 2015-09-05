b3f644d493799d44b897accc41e674e3fd957e89
inshal <inshal@inshal-PC>
2015-09-05 18:16:12 -0400

    new version

--- CoreJavaTraining/src/SecondProj.java
+++ CoreJavaTraining/src/SecondProj.java
@@ -3,11 +3,7 @@ public class SecondProj {
 
 	public static void main(String[] args) {
 		System.out.println("This is my 1st new file");
-		System.out.println("This is my 2nd new file");
-		System.out.println("This is my 3rd new file");
-		System.out.println("This is my 4th new file");
-		System.out.println("This is my 5th new file");
-		System.out.println("This is my 6th new file");
+		
 	}
 
 }
