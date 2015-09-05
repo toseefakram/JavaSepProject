7c690d4bda657acae5513c07570e448c325415c8
inshal <inshal@inshal-PC>
2015-09-05 17:25:50 -0400

    2nd commit

--- CoreJavaTraining/src/HelloStudents.java
+++ CoreJavaTraining/src/HelloStudents.java
@@ -2,6 +2,7 @@
 public class HelloStudents {
 	 public static void main(String args[]){
 		    System.out.println("Hello How are you: "+ args[0]+ " " +args[1]);
+		    System.out.println("i am fine bahe: ");
 		    //System.out.println("Hello How are you: "+ args[0]+ " " +args[1]+ " " + (2+5));
 	}
 
