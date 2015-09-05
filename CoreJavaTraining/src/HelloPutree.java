907e5d64b36b773cd6b2802fb6f579a2f59ec6d7
inshal <inshal@inshal-PC>
2015-09-05 18:27:21 -0400

    1st commit

---
+++ CoreJavaTraining/src/HelloPutree.java
@@ -0,0 +1,9 @@
+
+public class HelloPutree {
+
+	public static void main(String[] args) {
+		System.out.println("this is my putree file");
+
+	}
+
+}
---
+++ Dosra/src/NumTwo.java
@@ -0,0 +1,9 @@
+
+public class NumTwo {
+
+	public static void main(String[] args) {
+		System.out.println("This dosra proj");
+
+	}
+
+}
